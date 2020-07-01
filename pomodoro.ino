16203eb43163d39c814752c3a728947e0ad7293a
Tor Ivan <urbanthor@gmail.com>
2020-07-01 17:26:28 +0200

    added mario theme music

--- pomodoro.ino
+++ pomodoro.ino
@@ -28,17 +28,10 @@ bool start = false;
 unsigned long startMillis;
 unsigned long currentMillis;
 
+int work = 0;
 
 
-// notes in the melody:
-int melody[] = {
-  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
-};
 
-// note durations: 4 = quarter note, 8 = eighth note, etc.:
-int noteDurations[] = {
-  4, 8, 8, 4, 4, 4, 4, 4
-};
 
 
 void setup() {
@@ -48,10 +41,65 @@ void setup() {
   pinMode(buzzer, OUTPUT);
 }
 
+//Mario main theme melody
+int melody[] = {
+  NOTE_E7, NOTE_E7, 0, NOTE_E7, 
+  0, NOTE_C7, NOTE_E7, 0,
+  NOTE_G7, 0, 0,  0,
+  NOTE_G6, 0, 0, 0, 
 
+  NOTE_C7, 0, 0, NOTE_G6, 
+  0, 0, NOTE_E6, 0, 
+  0, NOTE_A6, 0, NOTE_B6, 
+  0, NOTE_AS6, NOTE_A6, 0, 
+
+  NOTE_G6, NOTE_E7, NOTE_G7, 
+  NOTE_A7, 0, NOTE_F7, NOTE_G7, 
+  0, NOTE_E7, 0,NOTE_C7, 
+  NOTE_D7, NOTE_B6, 0, 0,
+
+  NOTE_C7, 0, 0, NOTE_G6, 
+  0, 0, NOTE_E6, 0, 
+  0, NOTE_A6, 0, NOTE_B6, 
+  0, NOTE_AS6, NOTE_A6, 0, 
+
+  NOTE_G6, NOTE_E7, NOTE_G7, 
+  NOTE_A7, 0, NOTE_F7, NOTE_G7, 
+  0, NOTE_E7, 0,NOTE_C7, 
+  NOTE_D7, NOTE_B6, 0, 0
+};
+//Mario main them tempo
+int tempo[] = {
+  12, 12, 12, 12, 
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12, 
+
+  12, 12, 12, 12,
+  12, 12, 12, 12, 
+  12, 12, 12, 12, 
+  12, 12, 12, 12, 
+
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+};
 
 void loop() {
+ 
 
+       
   //delay(1000);
   // melodyNote();
   currentMillis = millis();
@@ -65,17 +113,22 @@ void loop() {
 
   if (buttonState != lastButtonState) {
     if (buttonState == HIGH) {
-      start = true;
+      
+      work = 1;
       setTime(0, 0, 0, 1, 1, 2010);
     }
   };
 
   lastButtonState = buttonState;
 
-  if (start) {
+  if (work == 1) {
 
     tomato();
   }
+  if (work == 2) {
+    workBreak();
+  }
+  
 }
 
 
@@ -89,35 +142,43 @@ void tomato() {
   if (tomatoSec < 10) {
     sec = 0 + sec;
   }
-  /*if (tomatoMin < 10 ) {
-    mins = 0 + mins;
-    }*/
+
 
   display.println( mins + ":" + sec );
 
   if ((mins == "0") && (sec == "00")) {
-    display.clearDisplay();
-    display.print("tomato");
-    tone(buzzer, 260, 500);
-
+    work = 2;
   }
 }
-/*
-  void melodyNote() {
-  // iterate over the notes of the melody:
-  for (int thisNote = 0; thisNote < 8; thisNote++) {
 
-    // to calculate the note duration, take one second divided by the note type.
-    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-    int noteDuration = 1000 / noteDurations[thisNote];
-    tone(8, melody[thisNote], noteDuration);
+void workBreak() {
 
-    // to distinguish the notes, set a minimum time between them.
-    // the note's duration + 30% seems to work well:
-    int pauseBetweenNotes = noteDuration * 1.30;
-    delay(pauseBetweenNotes);
-    // stop the tone playing:
-    noTone(8);
-  }
-  }
-*/
+    playMario();
+
+  
+  display.clearDisplay();
+  display.print("tomato");
+  work = 1;
+}
+
+void playMario() {
+   int size = sizeof(melody) / sizeof(int);
+     for (int thisNote = 0; thisNote < size; thisNote++) {
+
+       // to calculate the note duration, take one second
+       // divided by the note type.
+       //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+       int noteDuration = 1000/tempo[thisNote];
+
+       tone(buzzer, melody[thisNote],noteDuration);
+
+       // to distinguish the notes, set a minimum time between them.
+       // the note's duration + 30% seems to work well:
+       int pauseBetweenNotes = noteDuration * 1.30;
+       delay(pauseBetweenNotes);
+
+       // stop the tone playing:
+       tone(buzzer, 0,noteDuration);
+
+    }
+}
